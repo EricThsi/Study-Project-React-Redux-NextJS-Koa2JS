c59e636f5854eef492382972d8e150661f044afb
EricThsi <beefjava@gmail.com>
2020-04-22 16:53:11 +0800

    feat(styles): add styled-component to project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 import { Button } from 'antd';
 import './index.scss';
 
+const Title = styled.h1`
+  color: yellow;
+  font-size: 20px;
+`;
+
 export default () => (
-  <Link href='/a'>
-    <Button>Hello world! NextJS</Button>
-  </Link>
+  <div>
+    <Title>NextJS</Title>
+    <Link href='/a'>
+      <Button>Hello world! NextJS</Button>
+    </Link>
+  </div>
 );
