573dc98dfcf93c95fae951549b95db376cff4c77
EricThsi <beefjava@gmail.com>
2020-04-24 15:41:05 +0800

    feat(session): add session support

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const next = require('next');
 const dotenv = require('dotenv');
+const session = require('koa-session');
 
 const env = process.env.NODE_ENV;
 const isDev = env !== 'production';
@@ -17,6 +18,25 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+
+  server.keys = [process.env.APP_KEY];
+  const SESSION_CONFIG = {
+    key: 'app:sess',
+  };
+
+  server.use(session(SESSION_CONFIG, server));
+
+  server.use(async (ctx, next) => {
+    if (!ctx.session.user) {
+      ctx.session.user = {
+        name: 'thsi',
+      };
+    } else {
+      console.log('session is', ctx.session);
+    }
+
+    await next();
+  });
 
   server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res);
