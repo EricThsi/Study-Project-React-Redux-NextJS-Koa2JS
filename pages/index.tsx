2552e8281610717986c16dc3621d152d63dbc775
EricThsi <beefjava@gmail.com>
2020-04-27 17:04:36 +0800

    feat(users): render user info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { Button } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
@@ -16,31 +17,72 @@ const Title = styled.h1`
   font-size: 20px;
 `;
 
-const Index = ({ isLogin, userRepos, userStarring }) => {
+const Index = ({ user, isLogin, userRepos, userStarring }) => {
   console.log(userRepos);
   console.log(userStarring);
   console.log(isLogin);
-  // useEffect(() => {
-  //   axios.get('/api/user/info').then((res) => console.log(res));
-  // }, []);
 
+  if (!isLogin) {
+    return (
+      <div>
+        <p>Please Login.</p>
+        <Button type='primary' href={publicRuntimeConfig.OAUTH_URL}>
+          Github Login
+        </Button>
+      </div>
+    );
+  }
   return (
-    <div>
-      <Title>NextJS</Title>
-      <Link
-        href={{
-          pathname: '/detail',
-          query: {
-            name: 'user',
-          },
-        }}
-        passHref
-      >
-        <a>Detail</a>
-      </Link>
-      <p>
-        <a href={publicRuntimeConfig.OAUTH_URL}>Github Login</a>
-      </p>
+    <div className='page-wrapper'>
+      <style jsx>{`
+        .page-wrapper {
+          display: flex;
+          align-items: flex-start;
+        }
+
+        .user-info {
+          display: flex;
+          flex-direction: column;
+          width: 200px;
+          margin-right: 20px;
+          flex-shrink: 0;
+        }
+
+        .avatar {
+          width: 180px;
+          min-height: 100%;
+          border: 1px solid #ccc;
+          border-radius: 5px;
+        }
+
+        .login {
+          font-weight: 800;
+          font-size: 20px;
+        }
+      `}</style>
+      <div className='user-info'>
+        <img src={user.avatar_url} alt='User avatar' className='avatar' />
+        <p className='login'>{user.login}</p>
+        <p className='name'>{user.name}</p>
+        <p className='bio'>{user.bio}</p>
+        <p className='email'>
+          <MailOutlined />
+          <a href={`mailto:${user.email}`}>{user.email}</a>
+        </p>
+      </div>
+      <div className='user-repos'>
+        <Link
+          href={{
+            pathname: '/detail',
+            query: {
+              name: 'user',
+            },
+          }}
+          passHref
+        >
+          <a>Detail</a>
+        </Link>
+      </div>
     </div>
   );
 };
@@ -48,9 +90,10 @@ const Index = ({ isLogin, userRepos, userStarring }) => {
 Index.getInitialProps = async ({ ctx, reduxStore }) => {
   // reduxStore.dispatch(add(10));
   const { user } = reduxStore.getState();
-  if (!user || !user.id) {
+  if (!(user && user.id)) {
     return {
-      isLogin: false,
+      userRepos: {},
+      userStarring: {},
     };
   }
 
@@ -64,7 +107,6 @@ Index.getInitialProps = async ({ ctx, reduxStore }) => {
       req,
       res
     )
-    // .then((res) => console.log(res))
     .catch((err) => console.error(err));
 
   const userStarring = await api
@@ -78,22 +120,21 @@ Index.getInitialProps = async ({ ctx, reduxStore }) => {
     .catch((err) => console.error(err));
 
   return {
-    isLogin: true,
     userRepos: userRepos.data,
     userStarring: userStarring.data,
   };
 };
 
 function mapStateToProps(state) {
+  const { user } = state;
   return {
-    // counter: state.counter.counter,
+    user,
+    isLogin: !!(user && user.id),
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    add: (counter) => dispatch({ type: 'ADD', payload: { counter } }),
-  };
+  return {};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
